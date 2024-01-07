26e9982d6e15a76f2c13d111765c9bea77f8ee60
Zakhar Kubarev <j0kercoderscript@gmail.com>
2024-01-07 14:22:22 +0300

    Счетчик входов на сайт уникальный для каждого пользователя

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -6,17 +6,28 @@ import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        System.out.println("User successfully connected!");
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count") ;
 
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+        if (count == null){
+            session.setAttribute("count", 1);
+            count = 1;
+        }
+        else {
+            session.setAttribute("count", ++coun
+        }
+
+
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
 
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
-        pw.println("<h1> Hello "+ name + " " + surname + "!</h1>");
+//        pw.println("<h1> Hello "+ name + " " + surname + "!</h1>");
+        pw.println("<h1> Your count is: " + count + " </h1>");
         pw.println("</html>");
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/testJsp.jsp");
-        dispatcher.forward(request,response);
+
+
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
